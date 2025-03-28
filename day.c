2cbd7486d0c275258e6e7d07b30c9849664e2b28
Daniel Miller <daniel@wants.email>
2025-03-28 16:35:59 -0500

    Can code ever be too cute or too clear?
    (No, but code can be too clever and too complicated.)

--- day.c
+++ day.c
@@ -2,17 +2,108 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+  In theory, a character is the smallest meaningful unit of text,
+  like a letter or digit.
+
+  Unfortunately, C promises to recognize only a small handful of
+  distinct characters, typically just 255.
+
+  That's a big problem: The Chinese langauge alone has tens of
+  thousands of distinct characters. C characters just aren't big
+  enough to represent the world's languages.
+
+  Thus, text encodings like "UTF-8" were born. Behind the scenes,
+  these encodings represent real-world characters (like 天）by
+  bundling together multiple C characters.
+
+  These bundles of C characters are big enough to represent
+  over a million distinct real-life characters.
+
+  To minimize confusion, within these bundles, each C character
+  is typically referred to as a "byte" rather than a character.
+
+  (This character type represents a C character.)
+*/
 typedef char Character;
+
+/*
+  Text is a bunch of characters strung together.
+
+  In C, "a bunch of items strung together" are represented with
+  "pointers".
+
+  You can think of a pointer like it's a locker number. A locker
+  number points to one specific locker among many lockers, right?
+  Likewise, a pointer points to a specific item among many items.
+
+  In C, text is almost always represented by a character pointer
+  pointing to the text's first character.
+
+  To find to the text's second character, we add 1 to our
+  pointer's value, and so on. (Likewise, we add 1 to a locker
+  number to find its neighbor.)
+
+  Note that C pointers have many, many uses. They always point to
+  items, but they don't always represent a bunch of items strung
+  together.
+*/
+typedef Character *Text;
+
+// A bundle of C characters representing a real-life character
+// (like 天).
+typedef Character *CharacterBundle;
+
+// The numeric identifier, or "code", of a UTF-8 character.
 typedef uint32_t UTF8Codepoint;
+
+// Some number of items. Whole numbers only, zero or greater.
 typedef size_t Count;
-typedef size_t Index;
+
+/*
+  Given multiple items, an offset indicates which item we're
+  referring to. A value of 0 indicates the first item, 1 is the
+  second item, 2 is the third item, etc.
+
+  Whole numbers only, zero or greater.
+
+  Offsets sound a little like pointers, right? Yes, they're
+  related! If we think of a pointer as representing a locker
+  number, then an offset represents how many lockers to its right
+  is the locker we actually want to open.
+
+  In other words: Pointers are absolute; offsets are relative.
+
+  Does that make some sense?
+*/
+typedef size_t Offset;
+
+// The answer to a yes/no question.
 typedef bool YesNo;
+
+// Positive or negative whole numbers.
+typedef int Integer;
+
+/*
+  In C, we have to announce a function's signature before we can
+  use the function.
+
+  That's what this next little block is for!
+*/
 
 Count UTF8CharacterWidth(const Character *character);
 UTF8Codepoint UTF8CharacterCodepoint(const Character *character, Count character_w);
 YesNo IsUTF8CharacterChinese(UTF8Codepoint codepoint);
 
-int main(void)
+/*
+  When our program runs, C calls this function. Thus, this is our
+  program's "entry point".
+
+  C is very strict about this function! It must be named "main"
+  with all lowercase letters, it must return an integer, and its
+  arguments must have specific types.
+*/
+Integer main(Integer argument_count, Text arguments[])
 {
   // "Hello, world!"
   const auto greeting = u8"你好，世界！yawn";
@@ -20,11 +111,11 @@ int main(void)
   // How many bytes wide is the greeting?
   const auto greeting_w = strlen(greeting);
 
-  // The index of the current character in our greeting.
-  Index greeting_i = 0;
-  while (greeting_i < greeting_w)
+  // The offset of the current character in our greeting.
+  Offset greeting_o = 0;
+  while (greeting_o < greeting_w)
   {
-    const auto character = greeting + greeting_i;
+    const auto character = greeting + greeting_o;
     const auto character_w = UTF8CharacterWidth(character);
 
     const auto codepoint = UTF8CharacterCodepoint(character, character_w);
@@ -32,7 +123,7 @@ int main(void)
 
     /*
       Example output:
-      
+
       你      (0x00004f60)   Chinese!
       好      (0x0000597d)   Chinese!
       ，      (0x0000ff0c)   Chinese!
@@ -46,13 +137,14 @@ int main(void)
     */
     printf(
       "%.*s\t(0x%08x)   %s\n",
-      (int)character_w, character,
+      (Integer) character_w, character,
       codepoint,
       is_chinese ? "Chinese!" : "Booooooring.");
 
-    greeting_i += character_w;
+    greeting_o += character_w;
   }
 
+  // By returning 0, we tell C "We finished successfully!"
   return 0;
 }
 
@@ -91,8 +183,8 @@ Count UTF8CharacterWidth(const Character *character)
 
 // What is the codepoint of this UTF-8 character?
 UTF8Codepoint UTF8CharacterCodepoint(
-    const Character *character,
-    const Count character_w)
+  const Character *character,
+  const Count character_w)
 {
   /*
     UTF-8 characters can consist of 1, 2, 3, or 4 bytes.
@@ -122,7 +214,7 @@ UTF8Codepoint UTF8CharacterCodepoint(
       // As you can see above, the 7 x's for 1-byte characters
       // are already squished together, exactly where we need
       // 'em.
-      return (UTF8Codepoint) *character;
+      return *character;
     }
 
     case 2:
@@ -185,7 +277,7 @@ UTF8Codepoint UTF8CharacterCodepoint(
 }
 
 // Is this UTF-8 character codepoint Chinese?
-bool IsUTF8CharacterChinese(const UTF8Codepoint codepoint)
+YesNo IsUTF8CharacterChinese(const UTF8Codepoint codepoint)
 {
   // https://www.unicode.org/charts/
   return
@@ -207,3 +299,4 @@ bool IsUTF8CharacterChinese(const UTF8Codepoint codepoint)
     || (codepoint == 0x0FF1B)                         // Fullwidth semicolon
     || (codepoint == 0x0FF1F);                        // Fullwidth question mark
 }
+
